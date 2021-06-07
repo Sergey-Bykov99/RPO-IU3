13f41adf7740a52a68d68fd6bbb0bb544516d3c2
Сергей <sergej@Lyrical-Assault.local>
2021-06-07 17:13:35 +0300

    laba5-9

---
+++ src/main/java/rpo/backend/models/Country.java
@@ -0,0 +1,26 @@
+package rpo.backend.models;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Entity
+@Table(name = "contries")
+@Access(AccessType.FIELD)
+public class Country {
+
+    public Country() { }
+    public Country(Long id) { this.id = id; }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false, nullable = false)
+    public long id;
+
+    @Column(name = "name", nullable = false, unique = true)
+    public String name;
+
+    @OneToMany(mappedBy = "country")
+    public List<rpo.backend.models.Artist> artists = new ArrayList<rpo.backend.models.Artist>();
+}
